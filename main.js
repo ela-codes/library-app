05af146acb2a11b42ef7c54b8a445bdd4a0c965e
ateodocio <teodocio.am@gmail.com>
2023-01-13 16:02:26 -0600

    Update library table display and script

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 // GLOBAL VARIABLES
 const myLibrary = []; // contains books as objects
 let bookNum = 1
@@ -8,6 +9,7 @@ const closeModal = document.querySelector('.close-modal');
 const addButton = document.querySelector('#add-book');
 const form = document.querySelector('form');
 const bookIdNumber = document.querySelector('.book-id');
+const libraryDisplay = document.querySelector('.library-display');
 
 
 // FUNCTIONS
@@ -19,11 +21,12 @@ function setBookIdMessage() {
 }
 
 
-function Book(name, author, pages) {
+function Book(name, author, pages, status) {
     this.name = name
     this.author = author
     this.pages = pages
-    this.bookId = bookNum
+    this.status = status
+    this.bookId = bookNum - 1
 }
 
 function createNewBook() {
@@ -46,7 +49,26 @@ function hideModal() {
     modal.style.display = 'none';
 }
 
+function createBookCard(bookValues) {
+    // create new div element, new span elements, then insert text in those span elements
+    const newCard = document.createElement('div');
+    newCard.classList.add(`book-${bookNum-1}`);
+    libraryDisplay.appendChild(newCard);
+    
+    const bookName = document.createElement('span');
+    const bookAuthor = document.createElement('span');
+    const bookPages = document.createElement('span');
+    const bookStatus = document.createElement('span');
+    
+    const element = [bookName, bookAuthor, bookPages, bookStatus];
+    const attribute = ['name', 'author', 'pages', 'status'];
 
+    for (let j = 0; j < 4; j++) {
+        element[j].innerText = bookValues[j];
+        element[j].classList.add(attribute[j]);
+        newCard.appendChild(element[j]) 
+    }
+}
 
 
 // EVENT LISTENERS
@@ -55,12 +77,36 @@ openModal.addEventListener('click', () => {
     modal.style.display = 'block';
     setBookIdMessage()})
 
-closeModal.addEventListener('click', hideModal())
+closeModal.addEventListener('click', () => {hideModal()})
 
 addButton.onclick = (e) => {
     e.preventDefault()
     addBookToLibrary(createNewBook())
+    createBookCard(Object.values(myLibrary[myLibrary.length-1]))
     hideModal()
     form.reset()
-    bookNum += 1 // increment book id number
+    bookNum++ // increment book id number
 }
+
+const myBook1 = ['Psychology of Money', 'Morgan Housel', 256, 'Want to Read']
+const myBook2 = ['The Alchemist', 'Paulo Coelho', 175, 'Completed']
+myLibrary.push(new Book(myBook1[0], myBook1[1], myBook1[2], myBook1[3]))
+bookNum++
+myLibrary.push(new Book(myBook2[0], myBook2[1], myBook2[2], myBook2[3]))
+bookNum++
+console.log(myLibrary)
+
+
+// SAMPLE BOOK OBJECTS
+
+
+// openModal.addEventListener('click', () => {
+//     myLibrary.push(new Book(myBook1[0], myBook1[1], myBook1[2], myBook1[3]))
+//     console.log(myLibrary)
+//     myLibrary.push(new Book(myBook2[0], myBook2[1], myBook2[2], myBook2[3]))
+//     console.log(myLibrary)
+//     for (let i = myLibrary.length ; i > 0; i--) {
+//         createBookCard()
+//     }
+
+// })
